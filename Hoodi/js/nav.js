a6544d4c00503888dd722ce1754e7ae3bc22bac0
Tim Oster <obsilp@teamdna.de>
2018-11-02 18:43:52 +0100

    Basic nav

---
+++ Hoodi/js/nav.js
@@ -0,0 +1,19 @@
+$(document).ready(() => {
+    $("#button-logout").on("click", () => firebase.auth().signOut().then(() => changePage("login")));
+});
+
+function changePage(name) {
+    $(".page.active").removeClass("active");
+
+    const page = $("#page-" + name);
+    const title = page.data("title");
+
+    page.addClass("active");
+    $("#title").html(title);
+
+    $("#status-bar, #navigation-bar").toggleClass("invisible", title.length == 0);
+}
+
+function initContent() {
+    $("#page-main-username").html(userData.displayName);
+}
